eee11bff731042c687896c7b2cf0841439b88831
alphaxadPython <alphaxadcoder@gmail.com>
2021-12-14 19:13:51 +0300

    alpha Jozee

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
-(function ($) {
+(function($) {
     "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
     // Initiate the wowjs
     new WOW().init();
 
@@ -37,23 +26,23 @@
 
 
     // Smooth scrolling to section
-    $(".btn-scroll").on('click', function (event) {
+    $(".btn-scroll").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
-            
+
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 0
             }, 1500, 'easeInOutExpo');
         }
     });
-    
-    
+
+
     // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
+    $('.skill').waypoint(function() {
+        $('.progress .progress-bar').each(function() {
             $(this).css("width", $(this).attr("aria-valuenow") + '%');
         });
-    }, {offset: '80%'});
+    }, { offset: '80%' });
 
 
     // Portfolio isotope and filter
@@ -61,11 +50,11 @@
         itemSelector: '.portfolio-item',
         layoutMode: 'fitRows'
     });
-    $('#portfolio-flters li').on('click', function () {
+    $('#portfolio-flters li').on('click', function() {
         $("#portfolio-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        portfolioIsotope.isotope({ filter: $(this).data('filter') });
     });
 
 
@@ -77,19 +66,18 @@
         loop: true,
         items: 1
     });
-    
-    
+
+
     // Back to top button
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
         }
     });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 })(jQuery);
-
